eb00e8ed6784fe72ffbfd3dad551469f0ef68354
J2D2Development <joedriscoll79@gmail.com>
2015-03-26 10:15:07 -0700

    Adding jq effects to homepage, working on comments

--- contactus.php
+++ contactus.php
@@ -1,13 +1,15 @@
 <?php
 //process contact form - simple email send
 if(isset($_POST['submit'])) {
-    $to = "joedriscoll79@yahoo.com";
-    $from = $_POST['email'];
+    $to = "joedriscoll79@gmail.com";
+    $from = "joedriscoll79@yahoo.com";
     $subject = "Question about the wedding...";
     $message = $_POST['message'];
 
-    $headers = "From: " . $from;
-    $headers2 = "From:" . $to;
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= "From: <" . $from . ">\r\n";
+
     if(mail($to, $subject, $message, $headers)) {
         echo "Thanks for sending... <a href='home.html'>Click here to go home</a>";
     } else {
