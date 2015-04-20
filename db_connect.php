9c24cc763394cf9131aa60cf2f1c8c2df5d0774a
Joseph Driscoll <joedriscoll79@gmail.com>
2015-04-19 17:15:23 -0700

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //local connection
-    //$con = mysqli_connect('localhost', 'root', 'jcd2829', 'wedding');
-    //production database - at 104.236.39.224:3306
-    $con = mysqli_connect('localhost', 'root', 'aobjcdpdx', 'guestbook');
-?>
