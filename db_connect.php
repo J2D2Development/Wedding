62c1c5f5a8aea44a56842591d62f7a91f7209dce
J2D2Development <joedriscoll79@gmail.com>
2015-04-19 17:13:51 -0700

    Fixed connection to guestbook

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
     //local connection
     //$con = mysqli_connect('localhost', 'root', 'jcd2829', 'wedding');
-    //production database
-    $con = mysqli_connect('104.236.39.224:3306', 'root', 'aobjcdpdx', 'guestbook');
+    //production database - at 104.236.39.224:3306
+    $con = mysqli_connect('localhost', 'root', 'aobjcdpdx', 'guestbook');
 ?>
