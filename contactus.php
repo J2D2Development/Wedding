028672bf24d5f774df555c6e7afb0fada322cdd9
J2D2Development <joedriscoll79@gmail.com>
2015-05-03 14:11:58 -0700

    Fixing contact form, reorg homepage

--- contactus.php
+++ contactus.php
@@ -1,21 +1,24 @@
 <?php
 //process contact form - simple email send
-if(isset($_POST['submit'])) {
-    $to = "joedriscoll79@gmail.com";
-    $from = "questions@driscollwedding.com";
-    $subject = "Question about the wedding...";
-    $message = "<p>" . $_POST['message'] . "</p><p>" . "From: " . $_POST['name'] . "<br />" . "At: " . $_POST['email'] . "</p>";
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= "From: <" . $from . ">\r\n";
+$msg = $_POST['message'];
+$name = $_POST['name'];
+$from_email = $_POST['email'];
 
-    if(mail($to, $subject, $message, $headers)) {
-        echo "Thanks for sending... <a href='index.html'>Click here to go home</a>";
-    } else {
-        echo "Sorry, there's been an error!  Please contact Joe directly with any questions at joedriscoll79@gmail.com";
-        echo "<br /><br />";
-        echo "<a href='index.html'>Click here to go home</a>";
-    }
+$to = "joedriscoll79@gmail.com";
+$from = "questions@driscollwedding.com";
+$subject = "Question about the wedding...";
+$message = "<p>" . $msg . "</p><p>" . "From: " . $name . "<br />" . "At: " . $from_email . "</p>";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: <" . $from . ">\r\n";
+
+if(@mail($to, $subject, $message, $headers)) {
+    echo "Thanks for contacting us!<br />";
+    echo "Someone will be in touch soon to address your concerns  This pop up will close in 7 seconds, or click anwhere outside it to close.";
+} else {
+    echo "Sorry, there's been an error!  Please contact Joe directly with any questions at joedriscoll79@gmail.com";
 }
+
 ?>
