bbdd0f66ff3023e74cb8d2b5d052e7549533e752
J2D2Development <joedriscoll79@gmail.com>
2015-05-08 13:12:57 -0700

    Fixed contact form!  Also fixed homepage formatting

--- contactus.php
+++ contactus.php
@@ -16,7 +16,7 @@ $headers .= "From: <" . $from . ">\r\n";
 
 if(@mail($to, $subject, $message, $headers)) {
     echo "Thanks for contacting us!<br />";
-    echo "Someone will be in touch soon to address your concerns  This pop up will close in 7 seconds, or click anwhere outside it to close.";
+    echo "Someone will be in touch soon to address your concerns  This pop up will close momentarily, or click anwhere outside it to close.";
 } else {
     echo "Sorry, there's been an error!  Please contact Joe directly with any questions at joedriscoll79@gmail.com";
 }
